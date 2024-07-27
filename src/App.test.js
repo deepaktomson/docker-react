c60aaf922a3e07a0660a96b065c8f58176f4080e
Deepak <deepaktomson@gmail.com>
2024-07-27 19:53:17 +0530

    fixed tests in app.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React + Docker + Github Actions/i);
   expect(linkElement).toBeInTheDocument();
 });
 
